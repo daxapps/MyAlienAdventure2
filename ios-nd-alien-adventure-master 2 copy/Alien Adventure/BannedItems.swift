763ce18cace66de219f2d7e115f6e216a21d821d
daxapps <daxapps777@gmail.com>
2016-08-20 14:27:04 -0500

    BannedItems

--- ios-nd-alien-adventure-master 2 copy/Alien Adventure/BannedItems.swift
+++ ios-nd-alien-adventure-master 2 copy/Alien Adventure/BannedItems.swift
@@ -11,7 +11,26 @@ import Foundation
 extension Hero {
     
     func bannedItems(dataFile: String) -> [Int] {
-        return [Int]()
+        
+        var banned = [Int]()
+        
+        let itemPlistURL = NSBundle.mainBundle().URLForResource("ItemList", withExtension: "plist")!
+        
+        let itemsArray = NSArray(contentsOfURL: itemPlistURL) as! [[String:AnyObject]]
+        
+        for item in itemsArray {
+            if let itemID = item["ItemID"] as? Int {
+                if let name = item["Name"] as? String where name.localizedCaseInsensitiveContainsString("laser") {
+                    if let historicalData = item["HistoricalData"] as? [String:AnyObject]  {
+                        if let carbonAge = historicalData["CarbonAge"] as? Int where carbonAge < 30 {
+                            banned.append(itemID)
+                        }
+                    }
+                }
+            }            
+        }
+        
+        return banned
     }
 }
 
